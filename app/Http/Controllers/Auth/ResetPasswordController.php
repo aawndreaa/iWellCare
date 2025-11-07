a3a2f17f0743758986b96927c651c5323cee0cda
Bernal Andrea Jamille <bernalandreajamille18@gmail.com>
2025-11-07 18:32:58 +0800

    Fix password reset by removing request object from view data and adding null coalescing

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -18,7 +18,7 @@ class ResetPasswordController extends Controller
      */
     public function showResetForm(Request $request, $token = null)
     {
-        return view('auth.passwords.reset', ['token' => $token, 'email' => $request->email, 'request' => $request]);
+        return view('auth.passwords.reset', ['token' => $token, 'email' => $request->email]);
     }
 
     /**
